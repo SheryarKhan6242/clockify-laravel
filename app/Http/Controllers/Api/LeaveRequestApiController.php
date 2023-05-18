c8f708528bc31de571d4ff22e739ff2be9d2e24d
Muhammad Asad <asadrafiq7447@gmail.com>
2023-05-18 16:31:45 +0500

    set the leave request issue

--- app/Http/Controllers/Api/LeaveRequestApiController.php
+++ app/Http/Controllers/Api/LeaveRequestApiController.php
@@ -50,7 +50,7 @@ class LeaveRequestApiController extends Controller
                 $filepath = 'uploads/' . $fileName;
                 $path = Storage::disk('public')->put($filepath, file_get_contents($request->file('media')));
                 $path = Storage::disk('public')->url($path);
-                $memberProfile->profile_photo = $fileName;
+                $$leave->media = $fileName;
             }
             $leave->save();
             
