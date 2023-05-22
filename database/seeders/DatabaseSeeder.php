a11c80666e7939a768d9155d608dae876fa34e68
SheryarKhan6242 <113693589+SheryarKhan6242@users.noreply.github.com>
2023-05-22 12:36:37 +0500

    fix typo seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,6 +25,6 @@ class DatabaseSeeder extends Seeder
         $this->call(MaritalStatusSeeder::class);
         $this->call(CheckinTypeSeeder::class);
         $this->call(RoleSeeder::class);
-        $this->call(BankAccountType::class);
+        $this->call(BankAccountTypeSeeder::class);
     }
 }
