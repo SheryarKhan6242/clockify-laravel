60c79eeaec2dd4ddbf579e238a009342d86af5bf
SheryarKhan6242 <113693589+SheryarKhan6242@users.noreply.github.com>
2023-05-23 12:35:22 +0500

    send status 200 in verify otp instead of 401

--- app/Http/Controllers/Api/PasswordApiController.php
+++ app/Http/Controllers/Api/PasswordApiController.php
@@ -89,7 +89,7 @@ class PasswordApiController extends Controller
             return response()->json(['success' => true,'status' => 200,'message' => 'Otp Verified Successfully!','user_id' => $user->id,'access_token' => $accessToken,
             ]);
         } else {
-            return response()->json(['success' => false,'status' => 401,'message' => 'Invalid Email/Username or OTP',], 401);
+            return response()->json(['success' => false,'status' => 200,'message' => 'Invalid Email/Username or OTP']);
         }
     }
 
